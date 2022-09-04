96bf06a4da5fbb97230e5ed1d0097c321f310974
mehta-deeksha <111343664+mehta-deeksha@users.noreply.github.com>
2022-09-04 16:33:16 +0530

    Adding new variables workers count and instance-type

--- variables.tf
+++ variables.tf
@@ -16,3 +16,12 @@ variable ""external_ip" {
         type = string
         default = "0.0.0.0/0"
 }
+variable "workers-count" {
+  type    = number
+  default = 1
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t3.micro"
+}
