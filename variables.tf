824c64a734f626027382c3ca92e20ff44270086c
mehta-deeksha <111343664+mehta-deeksha@users.noreply.github.com>
2022-09-04 16:52:08 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,21 +1,25 @@
 variable "profile" {
-        type = "string"
-        default = "default"
+  type    = string
+  default = "default"
 }
 
 variable "region-jenkins-master" {
-        type = "string"
-        default = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "region-jenkins-worker" {
-        type = "string"
-        default = "us-west-2"
+  type    = string
+  default = "us-west-2"
 }
-variable ""external_ip" {
-        type = string
-        default = "0.0.0.0/0"
+variable "external_ip" {
+  type    = string
+  default = "0.0.0.0/0"
 }
+
+
+#This chunk of template can also be put inside variables.tf for better segregation
+
 variable "workers-count" {
   type    = number
   default = 1
